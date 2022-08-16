6697ee82ffa1e0bd49de05e145e75bfb27bb5c3e
valo-cyber <abasval558@gmail.com>
2022-08-16 15:27:06 -0500

    commit

--- script.js
+++ script.js
@@ -10,19 +10,29 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 5;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
+
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No Number!";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector(".message").textContent = "🎉 Correct Number!";
     document.querySelector(".number").textContent = secretNumber;
 
+    if(score > highScore){
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
+
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📈 Too high!";
-      score = score - 1;
+      score--;
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = "😔 You lose!";
@@ -31,7 +41,7 @@ document.querySelector(".check").addEventListener("click", function () {
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
-      score = score--;
+      score--;
       document.querySelector(".score").textContent = score;
     } else {
       document.querySelector(".message").textContent = "😔 You lose!";
