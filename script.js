be47b2a343563978cabf5873f21416b971cc4d76
valo-cyber <abasval558@gmail.com>
2022-08-16 22:50:00 -0500

    refactor

--- script.js
+++ script.js
@@ -11,17 +11,22 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 5;
 let highScore = 0;
+const displayMessage = function(message){
+  document.querySelector(".message").textContent = message
+}
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
 
   // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No Number!";
+    // document.querySelector(".message").textContent = "⛔ No Number!";
+    displayMessage("⛔ No Number!")
 
     // When player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "🎉 Correct Number!";
+    // document.querySelector(".message").textContent = "🎉 Correct Number!";
+    displayMessage( "🎉 Correct Number!")
     document.querySelector(".number").textContent = secretNumber;
 
     if (score > highScore) {
@@ -30,12 +35,14 @@ document.querySelector(".check").addEventListener("click", function () {
     }
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent =
-        guess > secretNumber ? "📈 Too high!" : "📉 Too low!";
+      // document.querySelector(".message").textContent =
+      //   guess > secretNumber ? "📈 Too high!" : "📉 Too low!";
+      displayMessage( guess > secretNumber ? "📈 Too high!" : "📉 Too low!")
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "😔 You lose!";
+      // document.querySelector(".message").textContent = "😔 You lose!";
+      displayMessage("😔 You lose!")
       document.querySelector(".score").textContent = 0;
     }
   }
@@ -45,7 +52,8 @@ document.querySelector(".again").addEventListener("click", function () {
   score = 5;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
 
-  document.querySelector(".message").textContent = "Play😅...";
+  // document.querySelector(".message").textContent = "Play😅...";
+  displayMessage("Play😅...")
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = null;
