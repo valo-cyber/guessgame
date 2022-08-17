723a1de1953a8573480a9a749e738163558b4781
valo-cyber <abasval558@gmail.com>
2022-08-16 23:45:04 -0500

    modalCommit2

--- MODAL/script.js
+++ MODAL/script.js
@@ -8,5 +8,10 @@ const btnOpenModal = document.querySelectorAll(".show-modal");
 for(let i =0; i < btnOpenModal.length; i++){
     btnOpenModal[i].addEventListener("click", function(){
         modal.classList.remove("hidden")
+        overlay.classList.remove("hidden")
     })
-}
+};
+btnCloseModal.addEventListener("click", function(){
+    modal.classList.add("hidden")
+    overlay.classList.add("hidden")
+})
