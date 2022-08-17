af20533e0a920f064faf63b9f6f19d902fb7dc7c
valo-cyber <abasval558@gmail.com>
2022-08-16 23:47:04 -0500

    modalCommit3

--- MODAL/script.js
+++ MODAL/script.js
@@ -5,13 +5,20 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 
-for(let i =0; i < btnOpenModal.length; i++){
-    btnOpenModal[i].addEventListener("click", function(){
-        modal.classList.remove("hidden")
-        overlay.classList.remove("hidden")
-    })
+const openModal =  function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  }
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModal);
+}
+btnCloseModal.addEventListener("click", openModal);
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
 };
-btnCloseModal.addEventListener("click", function(){
-    modal.classList.add("hidden")
-    overlay.classList.add("hidden")
-})
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
