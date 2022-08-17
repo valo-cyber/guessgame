573cc96a6c361dd1f5c5a60b569cea0d8884c8e3
valo-cyber <abasval558@gmail.com>
2022-08-16 23:35:26 -0500

    modalCommit1

--- MODAL/script.js
+++ MODAL/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelectorAll(".show-modal");
+
+for(let i =0; i < btnOpenModal.length; i++){
+    btnOpenModal[i].addEventListener("click", function(){
+        modal.classList.remove("hidden")
+    })
+}
